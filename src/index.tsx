72a820e5093e47ad70b90892f8aa0bd13ff2f482
Артемий <artema121rus@mail.ru>
2024-05-06 15:13:37 +0300

    Создает функцию classNames для создания классов

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import App from "./App";
 import {BrowserRouter} from "react-router-dom";
 import ThemeProvider from "./theme/ThemeProvider";
 
+
 render(
     <BrowserRouter>
         <ThemeProvider>
